e9f12349057e3a42f95ccbcd8c7678787014bf1b
oleg-19952008 <oleg-19952008@mail.ru>
2026-08-01 16:38:53 +0300

    12

--- Messenger.Core.Server/Data/UserRepository.cs
+++ Messenger.Core.Server/Data/UserRepository.cs
@@ -1,6 +1,7 @@
 using Microsoft.Data.Sqlite;
+using Microsoft.Extensions;
+using Microsoft.Extensions.DependencyInjection; // <--- Добавить это
 using Messenger.Core.Models;
-
 namespace Messenger.Core.Data;
 
 /// <summary>
